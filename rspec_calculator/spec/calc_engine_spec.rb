32cdc2aaf1b22523e85e6dd70f97b5a588eefe39
Tech-A04 <tech-a04@MacBook-Air-7.local>
2018-04-10 09:52:12 +0100

    Added hook

--- rspec_calculator/spec/calc_engine_spec.rb
+++ rspec_calculator/spec/calc_engine_spec.rb
@@ -3,22 +3,24 @@ require 'spec_helper'
 
 describe 'Calcengine' do
 
-    calc = Calcengine.new
+    before(:each) do
+        @calc = Calcengine.new
+    end
 
     it 'should correctly add two number' do
-        expect(calc.add(1,1)).to eql(2)
+        expect(@calc.add(1,1)).to eql(2)
     end
 
     it 'should correctly multiply two numbers' do
-        expect(calc.multiply(1,1)).to eql(1)
+        expect(@calc.multiply(1,1)).to eql(1)
     end
 
     it 'should correctly divide two numbers' do
-        expect(calc.divide(1,1)).to eql(1)
+        expect(@calc.divide(1,1)).to eql(1)
     end
 
     it 'should correctly substract two numbers' do
-        expect(calc.substract(1,1)).to eql(0)
+        expect(@calc.substract(1,1)).to eql(0)
     end
 
 
