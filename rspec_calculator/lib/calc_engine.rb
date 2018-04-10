832e045ad63b1b82339c21ae9bd8917fefddc682
Tech-A04 <tech-a04@MacBook-Air-7.home>
2018-04-10 07:56:02 +0100

    Added homework

---
+++ rspec_calculator/lib/calc_engine.rb
@@ -0,0 +1,19 @@
+class Calcengine
+
+    def add(number1, number2)
+        number1 + number2
+    end
+
+    def multiply(number1, number2)
+        number1 * number2
+    end
+
+    def divide(number1, number2)
+        number1 / number2
+    end
+
+    def substract(number1, number2)
+        number1 - number2
+    end
+
+end
